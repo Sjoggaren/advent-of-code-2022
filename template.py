3529489a1c2a8506edffe033096ac0c9da4fe9d9
matsj024 <matsj024@student.liu.se>
2022-12-01 11:24:08 +0100

    Updated template with useful functions

--- template.py
+++ template.py
@@ -1,8 +1,37 @@
+import collections as coll
+import datetime as dt
+import itertools as it
+import math
+from operator import itemgetter as ig
+import pprint as pp
+import re
+from utils import *
+#from grid import Grid
 from aocd.models import Puzzle
+
+def solve(d):
+    print("input:, ", repr(d))
+    t = 0
+
+    #nums = ints(d)
+    #ww = words(d)
+
+    #for line in ww:
+    #    for word in line:
+
+    #inp = []
+    #out = []
+    v = vm(ints(inpt), inp, out)
+    
+    # nums = [int(x) for x in d.split("\t")]
+    # nums = [[int(x) for x in l.split("\t")] for l in d.split("\n")]
+    # for num in nums:
+
+    return t
 
 puzzle = Puzzle(2022, day)
 
-input_data = puzzle.input_data
+input = puzzle.input_data
 
 #puzzle.answer_a = 3
 #puzzle.answer_b = 4
