23091d9dd452bd54cfc9d99771f7da05d62b9b6c
Mattias <matsj024@student.liu.se>
2022-12-19 00:52:34 +0100

    day 16 part 1 done, with some help from a friend :)

--- Solutions/day16.py
+++ Solutions/day16.py
@@ -1,9 +1,4 @@
 import collections as coll
-import datetime as dt
-import itertools as it
-import math
-from operator import itemgetter as ig
-import pprint as pp
 import re
 import functools
 
@@ -11,66 +6,65 @@ import functools
 # from grid import Grid
 from aocd.models import Puzzle
 
+
 def bfs(start, dict):
-    pass
+    flows = []
+    vistited = set()
+    queue = coll.deque()
+
+    queue.append((start, 0))
+    vistited.add(start)
+
+    while queue:
+        s, dist = queue.popleft()
+
+        for neighbour in dict[s][1]:
+            if neighbour in vistited:
+                continue
+            queue.append((neighbour, dist + 1))
+            vistited.add(neighbour)
+            if dict[neighbour][0] > 0:
+                flows.append((dist + 1, neighbour, dict[neighbour][0]))
+
+    return flows
 
 
-def highest_pressure(start, steps, dict, memo, open):
-    # No moves left return flowrate of current valve
-    if steps == 0:
-        return 0
+def highest_pressure(start, start_steps, possible_moves):
+    @functools.cache
+    def inner(pos, steps, opened):
 
-    # Check if we already calculated this state
-    if (start, steps, open) in memo:
-        return memo[(start, steps, open)]
+        max_pressure = 0
 
-    max_pressure = 0
+        for dist, neighbour, flow in possible_moves[pos]:
+            if steps - dist - 1 <= 0 or neighbour in opened:
+                continue
+            pressure = inner(
+                neighbour, steps - dist - 1, opened | {neighbour}
+            ) + flow * (steps - dist - 1)
+            max_pressure = max(max_pressure, pressure)
 
-    # Take an action to turn valve
-    if steps > 0 and start not in open:
+        return max_pressure
 
-        
-        pressure = (
-            highest_pressure(start, steps - 1, dict, memo, open | {start})
-            + sum(dict[valve][0] for valve in open)
-        )
-        max_pressure = max(max_pressure, pressure)
-
-    for j in dict[start][1]:
-        # Take an action to go to another valve
-        pressure = highest_pressure(j, steps - 1, dict, memo, open) + sum(dict[valve][0] for valve in open)
-        max_pressure = max(max_pressure, pressure)
-
-    memo[(start, steps, open)] = max_pressure
-
-    return max_pressure
+    return inner(start, start_steps, frozenset(start))
 
 
 def solvep1(d):
-    print("input:, ", repr(d))
-    t = 0
-
     row = [x for x in d.split("\n")]
-    print(row)
-    # elem = [[x for x in l.split("\n")] for l in d.split("\n")]
-    flow_rates = coll.defaultdict()
-    
+    valves = coll.defaultdict()
+
     for r in row:
         flow_rate = [int(j) for j in re.findall("(\d+)", r)]
         current_tunnel = [ct[-2:] for ct in re.findall("Valve ?[A-Z]+", r)]
         leads_to = re.split("valve?.\s", r)
         for i, flow in enumerate(flow_rate):
-            flow_rates[current_tunnel[i]] = (
+            valves[current_tunnel[i]] = (
                 flow,
                 [path.strip() for path in leads_to[1].split(",")],
             )
-    steps = 30
-    memory = coll.defaultdict()
-    open = frozenset()
 
-    print(highest_pressure("AA", steps, flow_rates, memory, open))
+    possible_moves = {point: bfs(point, valves) for point in valves.keys()}
 
-    return t
+    return highest_pressure("AA", 30, possible_moves)
 
 
 def solvep2(d):
@@ -89,8 +83,8 @@ def solvep2(d):
 puzzle = Puzzle(2022, 16)
 input = puzzle.input_data
 
-#with open("test.txt", "r") as f:
-#    solvep1(f.read())
+with open("test.txt", "r") as f:
+    solvep1(f.read())
 
 print("Answer problem 1:", solvep1(input))
 # print("Answer problem 2", solvep2(input))
